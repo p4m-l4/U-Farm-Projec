d35eea9923409216c83c6ba822c8a0828f45073b
TrippleA-Ashaba <trippleaunit@hotmail.com>
2022-11-15 16:32:08 +0300

    One login page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
+
+router.get("/login", (req, res) => {
+	res.render("login");
+});
+
+router.post(
+	"/login",
+	passport.authenticate("local", { failureRedirect: "/login/login" }),
+	(req, res) => {
+		console.log(req.body);
+		req.session.user = req.user;
+		const user = req.session.user;
+		console.log(user.role);
+		res.send("Logged In as " + user.role);
+	}
+);
 
 router.get("/signIn", (req, res) => {
 	res.render("signIn");
 });
-router.post("/signIn", passport.authenticate('local', { failureRedirect: "/"}), (req, res) => {
-    console.log(req.body),
-	res.redirect("signInUrban");
+router.post("/signIn", passport.authenticate("local", { failureRedirect: "/" }), (req, res) => {
+	console.log(req.body), res.redirect("signInUrban");
 });
 
 router.get("/farmer", (req, res) => {
@@ -18,15 +33,12 @@ router.get("/urban", (req, res) => {
 	res.render("signInUrban");
 });
 
-router.post("/urban", passport.authenticate('local', { failureRedirect: "/urban"}), (req, res) => {
+router.post("/urban", passport.authenticate("local", { failureRedirect: "/urban" }), (req, res) => {
 	res.redirect("signInUrban");
 });
-
 
 router.get("/officer", (req, res) => {
 	res.render("signInAgrico");
 });
-
-
 
 module.exports = router;
